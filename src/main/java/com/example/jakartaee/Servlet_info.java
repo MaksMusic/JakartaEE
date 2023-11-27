e281d0f1068999a2e299cea5d2760bc3822e14e8
MaksMusic <Maksmusic05@yandex.ru>
2023-11-27 23:45:50 +0300

    Initial commit

---
+++ src/main/java/com/example/jakartaee/Servlet_info.java
@@ -0,0 +1,26 @@
+package com.example.jakartaee;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet(name = "Servlet1", value = "/Servlet1")
+public class Servlet_info extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("<html><body>");
+        pw.println("<h1>Info</h1>");
+        pw.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hello World!" %>
+</h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a>
+<br/>
+<a href="servlet-info">info</a>
+<br/>
+<a href="servlet_tasks">tasks</a>
+</body>
+</html>
